dbdb28890cd70730230c2015bc4b708f8acd775c
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-03-23 22:33:32 +1000

    Correct function is_valid_check

--- Prac04/totalIncome.py
+++ Prac04/totalIncome.py
@@ -4,20 +4,23 @@ Starter code for cumulative total income program
 """
 
 
-def main():
-    incomes = []
-    months = int(input("How many months? "))
-
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
-        incomes.append(income)
-
+def print_report(incomes, num_month):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, num_month + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
+
+def main():
+    incomes = []
+    num_month = int(input("How many months? "))
+
+    for month in range(1, num_month + 1):
+        income = float(input("Enter income for month {} : ".format(month)))
+        incomes.append(income)
+
+    print_report(incomes, num_month)
 
 main()
