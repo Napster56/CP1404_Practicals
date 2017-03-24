87161a2e5304c8414a8ac78ccc96534f7f6320e5
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-03-24 13:46:26 +1000

    Complete 'To Do'

--- Prac04/listComprehensions.py
+++ Prac04/listComprehensions.py
@@ -1,6 +1,9 @@
 """
+
 CP1404/CP5632 Practical
+
 List comprehensions
+
 """
 
 names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
@@ -25,7 +28,11 @@ print(full_initials)
 almost_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # TODO: use a list comprehension to create a list of integers from this list of strings
-# numbers =
+numbers = [int(num) for num in almost_numbers]
+print(numbers)
+
 
 # TODO: use a list comprehension to create a list of all of the full_names in lowercase
-# lowercase_full_names =
+
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
