8a3b8e2179ccc10ad26a01a4b4cabf0188c93db9
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-04-11 23:17:06 +1000

    Add files to Prac05

--- Prac05/birth_date_dict.py
+++ Prac05/birth_date_dict.py
@@ -2,23 +2,36 @@
 Program asks user for the date-of-birth details for 5 people, and displays their individual ages by using a dictionary.
 """
 
-#TODO Finish this!!!!
-
-birth_date_dict = {}
+import datetime
 
 
-def add_date(name, birth_date):
+
+def add_date(name, birth_date, birth_date_dict):
     birth_date_dict[name] = birth_date
-    print(birth_date_dict)
+
+
+def calc_age(birth_date):
+    day, month, year = birth_date.split("/")
+    birth_year = int(year)
+    this_year = datetime.datetime.now().year
+    age = this_year - birth_year
+    return age
+
+
+def print_ages(all_ages, age):
+    for name in all_ages:
+        print("{} is {} years old".format(name, age))
 
 
 def main():
-    name = input("Enter the person's name: ")
-    birth_date = input("Enter the person's date of birth: ")
-    for i in range(5):
-        add_date(name, birth_date)
+    birth_date_dict = {}
+    all_ages = []
+    for i in range(2):
         name = input("Enter the person's name: ")
         birth_date = input("Enter the person's date of birth: ")
-        i += 1
+        add_date(name, birth_date, birth_date_dict)
+        age = calc_age(birth_date)
+        print_ages(age, name)
+
 
 main()
