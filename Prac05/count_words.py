25ea5465d20fe01573d0b7ad5eaf0894aa4d3b56
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-04-13 10:31:51 +1000

    Add files to Prac05

--- Prac05/count_words.py
+++ Prac05/count_words.py
@@ -3,30 +3,49 @@ Program asks user for a string containing several words, then counts how many ti
 Text: this is a collection of words of nice words this is a fun thing it is
 """
 
-user_string = input("Enter the words for your string: ")
-string_list = user_string.split()
 
-count_word = {}
+def create_word_key_list(count_word, word_key_list):
+    for key, count in count_word.items():
+        word_key_list.append((key, count))
+        sorted_list = sorted(word_key_list)
+    print(sorted_list)
+    return sorted_list
 
-for word in string_list:
-    if word in count_word:
-        count_word[word] += 1
-    else:
-        count_word[word] = 1
 
-word_key_list = []
+def create_word_list(count_word, sorted_list):
+    for key, count in count_word.items():
+        sorted_list.append(key)
+        longest_word = max(sorted_list)
+        print(longest_word)
+        return longest_word
 
-for key, count in count_word.items():
-    word_key_list.append((key, count))
-word_key_list.sort()
-print(word_key_list)
 
-word_list = []
+def print_list(longest_word, sorted_list):
+    for key, word in sorted_list:
+        print("{:{}} : {}".format(key, len(longest_word), word))
 
-for key, count in count_word .items():
-    word_list.append(key)
-print(word_list)
-longest_word = max(word_list)  #TODO correct this!!!!!
 
-for key, word in word_key_list:
-    print("{:{}} : {}".format(key, len(longest_word), word))
+def main():
+    count_word = {}
+    word_key_list = []
+    word_list = []
+
+    user_string = input("Enter the words for your string: ")
+    string_list = user_string.split()
+    print(string_list)
+    for word in string_list:
+        if word in count_word:
+            count_word[word] += 1
+        else:
+            count_word[word] = 1
+    print(count_word)
+    create_word_key_list(count_word, word_key_list)
+    longest_word = create_word_list(count_word, word_list)
+    print_list(longest_word, word_key_list)
+
+main()
+
+
+
+
+
