85e99f05bd82bad86c1d485f33d00c68095678d5
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-04-14 12:19:45 +1000

    Add files to Prac05

--- Prac05/birth_date_dict.py
+++ Prac05/birth_date_dict.py
@@ -5,33 +5,35 @@ Program asks user for the date-of-birth details for 5 people, and displays their
 import datetime
 
 
-
-def add_date(name, birth_date, birth_date_dict):
-    birth_date_dict[name] = birth_date
+# def add_date(name, birth_date, birth_date_dict):
+#    birth_date_dict[name] = birth_date
 
 
 def calc_age(birth_date):
     day, month, year = birth_date.split("/")
+    birth_day = int(day)
+    birth_month = int(month)
     birth_year = int(year)
-    this_year = datetime.datetime.now().year
+    now = datetime.date.today()
+    this_year = now.year
     age = this_year - birth_year
+    if now.month < birth_month or now.month == birth_month and now.day < birth_day:
+        age = this_year - birth_year - 1
     return age
 
 
-def print_ages(all_ages, age):
-    for name in all_ages:
+def print_ages(birth_date_dict):
+    for name, age in birth_date_dict.items():
         print("{} is {} years old".format(name, age))
 
 
 def main():
     birth_date_dict = {}
-    all_ages = []
-    for i in range(2):
+    for i in range(5):
         name = input("Enter the person's name: ")
         birth_date = input("Enter the person's date of birth: ")
-        add_date(name, birth_date, birth_date_dict)
+        birth_date_dict[name] = birth_date
         age = calc_age(birth_date)
-        print_ages(age, name)
-
-
+        birth_date_dict[name] = age
+    print_ages(birth_date_dict)
 main()
--- Prac05/count_words.py
+++ Prac05/count_words.py
@@ -4,13 +4,45 @@ Text: this is a collection of words of nice words this is a fun thing it is
 """
 
 
+# Count words in string
+def build_dictionary(count_word, user_string):
+    string_list = user_string.strip().split()   # splits & strips string into words
+    for word in string_list:
+        if word in count_word:
+            count_word[word] += 1
+        else:
+            count_word[word] = 1
+    return count_word
+
+
+# Print format using longest word in list
+def print_list(longest_word, sorted_list):
+    for key, word in sorted_list:
+        print("{:{}} : {}".format(key, len(longest_word), word))
+
+
+def main():
+    count_word = {}
+    user_string = input("Enter the words for your string: ")
+    count_word = build_dictionary(count_word, user_string)
+    longest_word = max(count_word.keys(), key=len)
+    sorted_list =[(k, v) for k, v in count_word.items()]
+    sorted_list.sort()
+    print_list(longest_word, sorted_list)
+
+main()
+"""
+Program asks user for a string containing several words, then counts how many times each word appears in the string.
+Text: this is a collection of words of nice words this is a fun thing it is
+"""
+
+
 def create_word_key_list(count_word, word_key_list):
     for key, count in count_word.items():
         word_key_list.append((key, count))
         sorted_list = sorted(word_key_list)
     print(sorted_list)
     return sorted_list
-
 
 def create_word_list(count_word, sorted_list):
     for key, count in count_word.items():
