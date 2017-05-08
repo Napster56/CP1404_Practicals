8926609927ce129a4732e97ffe38e0fa6df06db4
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-05-08 22:21:48 +1000

    Update file taxi_simulator.py
    File taxi_simulator.py updates include:
    update functions main(), choose_taxi(), calculate_total_bill
    print statements

--- Prac08/taxi_simulator.py
+++ Prac08/taxi_simulator.py
@@ -1,6 +1,9 @@
 """
 CP1404 Practical 8 - Inheritance
 Client code to use the Taxi and SilverServiceTaxi class.
+Program asks the user to select from a menu; user can choose to quit, choose a taxi and drive
+the taxi. Program displays a list of available taxis, the trip cost, the bill to date and
+the total trip cost on quit.
 """
 
 from silver_taxi import SilverServiceTaxi
@@ -25,7 +28,7 @@ def get_choice():
 def display_taxi_list(taxis):
     print("Taxis available:")
     for i, taxi in enumerate(taxis):
-        print(i, taxi)
+        print(i, "-", taxi)
 
 
 def get_distance(my_taxi):
@@ -33,54 +36,51 @@ def get_distance(my_taxi):
     my_taxi.drive(distance)
 
 
-def choose_taxi(my_bill, taxis):
-    my_taxi = int(input("Choose taxi: "))
+def choose_taxi(my_taxi, taxis):
     if my_taxi == 0:
         my_taxi = taxis[0]
-        print("Bill to date: ${:.2f}".format(my_bill))
-        get_distance(my_taxi)
-        calculate_bill(my_taxi)
+        display_menu()
     elif my_taxi == 1:
         my_taxi = taxis[1]
-        print("Bill to date: ${:.2f}".format(my_bill))
-        get_distance(my_taxi)
-        calculate_bill(my_taxi)
+        display_menu()
     else:
         my_taxi = taxis[2]
-        print("Bill to date: ${:.2f}".format(my_bill))
-        get_distance(my_taxi)
-        calculate_bill(my_taxi)
+        display_menu()
+    return my_taxi
 
+
+def calculate_total_bill(my_taxi, total_bill):
+    total_bill += my_taxi.get_fare()
+    return total_bill
 
 
 def calculate_bill(my_taxi):
-    my_bill = my_taxi.get_fare()
-    print("Your {} trip cost you ${:.2f}".format(my_taxi.name, my_bill))
-    print("Bill to date: ${}".format(my_bill))
-    display_menu()
-    get_choice()
-
-
-def process_menu_selection(choice, taxis):
-    while choice != "q":
-        if choice == "c":
-            my_bill = 0
-            display_taxi_list(taxis)
-            choose_taxi(my_bill, taxis)
-        elif choice == "d":
-            trip_distance = get_distance()
-            return trip_distance
-
-    return
+    my_taxi.get_fare()
+    print("Your {} trip cost you ${:.2f}".format(my_taxi.name, my_taxi.get_fare()))
 
 
 def main():
+    total_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     display_menu()
     choice = get_choice()
-    my_taxi = process_menu_selection(choice, taxis)
-    calculate_bill(my_taxi)
+    while choice != "q":
+        if choice == "c":
+            display_taxi_list(taxis)
+            my_taxi = int(input("Choose taxi:"))
+            print("Bill to date: ${:.2f}".format(total_bill))
+            my_taxi = choose_taxi(my_taxi, taxis)
+            choice = get_choice()
+        elif choice == "d":
+            get_distance(my_taxi)
+            calculate_bill(my_taxi)
+            total_bill = calculate_total_bill(my_taxi, total_bill)
+            print("Bill to date: ${:.2f}".format(total_bill))
+            display_menu()
+            choice = get_choice()
 
+    print("Total trip cost: ${}".format(total_bill))
+    display_taxi_list(taxis)
 
 main()
