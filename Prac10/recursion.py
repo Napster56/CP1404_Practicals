c02b1cb4a8c5b109e24eebf3ab9c2c8555463c32
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-05-26 13:53:37 +1000

    Update file; complete exercise

--- Prac10/recursion.py
+++ Prac10/recursion.py
@@ -9,18 +9,32 @@ def do_it(n):
         return 0
     return n % 2 + do_it(n - 1)
 
+
+
 # TODO: 1. write down what you think the output of this will be,
+
 # TODO: 2. use the debugger to step through and see what's actually happening
-print(do_it(5))
+
+# print(do_it(5))
+
+
+
 
 
 def do_something(n):
     if n < 0:
-        print(n ** 2)
+        return 0
+    # print(n ** 2)
     do_something(n - 1)
+    print(n ** 2)
+
 
 # TODO: 3. write down what you think the output of this will be,
+
 # TODO: 4. use the debugger to step through and see what's actually happening
-# do_something(4)
+
+print(do_something(4))
+
+
 
 # TODO: 5. fix the do_something() function so that it works the way it is probably supposed to :)
