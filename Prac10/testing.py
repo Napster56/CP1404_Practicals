9d227eaf1d60cb3c83dde90c61bf7e0002b0eab8
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-05-26 14:51:10 +1000

    Update file; completed exercises 1 - 4

--- Prac10/testing.py
+++ Prac10/testing.py
@@ -4,15 +4,33 @@ Testing demo using assert and doctest
 """
 
 import doctest
-from Prac07.car import Car
 
+class Car:
+    def __init__(self, name='', fuel=0):
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
+
+    def add_fuel(self, amount):
+        self.fuel += amount
+
+    def drive(self, distance):
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
+
+    def __str__(self):
+        return "{},  fuel= {}, odometer= {}".format(self.name, self.fuel, self.odometer)
 
 def repeat_string(s, n):
     """
     repeat string s, n times, with spaces in between
     """
-    return s * n
-
+    return " ".join([s] * n)
 
 def is_long_word(word, length=5):
     """
@@ -24,12 +42,12 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
-
+    return len(word) >= length
 
 def run_tests():
     # assert test with no message - used to see if the function works properly
     assert repeat_string("Python", 1) == "Python"
+
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
@@ -37,27 +55,49 @@ def run_tests():
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message - used to see if Car's init method sets the odometer correctly
+
     # this should pass (no output)
+
     test_car = Car()
+
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways: using the value passed in or the default
-    # You should test both of these
-    test_car = Car(fuel=10)
 
+    # Note that Car's __init__ function sets the fuel in one of two ways: using the value passed in or the default
+
+    # You should test both of these
+
+    test_car = Car(fuel=10)
+    test_car.drive(5)
+    assert test_car.odometer == 5, "Car does not set odometer correctly"
+    test_car.drive(5)
+    assert test_car.odometer == 10, "Car does not set odometer correctly"
 
 run_tests()
 
+
+
 # TODO: 3. Uncomment the following line and run the doctests
-# doctest.testmod()
+
+doctest.testmod()
+
+
 
 # TODO: 4. Fix the failing is_long_word function (don't change the tests, but the function!)
 
+
+
 # TODO: 5. Write and test a function to format a phrase as a sentence - starting with a capital and ending with a single full stop
+
 # Important: start with a function header and just use pass as the body
+
 # then add doctests so that:
+
 # 'hello' -> ''Hello.'
+
 # 'It is an ex parrot.' -> 'It is an ex parrot.'
+
 # and one more you decide (that's valid!)
+
 # then write the body of the function so that the tests pass
