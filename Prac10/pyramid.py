a27fc0f76f823911665979f2df568529ea7e9f16
Tyrone Napoli <tyrone.napoli@my.jcu.edu.au>
2017-07-30 17:51:02 +1000

    Update file; complete docstring

--- Prac10/pyramid.py
+++ Prac10/pyramid.py
@@ -1,23 +1,29 @@
 """
-Program to ask user
+Write a program to get the number of rows from the user and calculate the number of blocks
+you will need given the number of rows (n) to make a 2D pyramid.
+
 """
 
-rows = int(input("How many rows?:"))
+rows = int(input("Enter the number of rows in the pyramid: "))
 
-# calculate pyramid blocks using while loop
-# sum = 0
-# while rows > 0:
-#     sum += rows
-#     rows -= 1
-# print(sum)
 
-# calculate pyramid blocks using recursion
+# using a for loop
+def calculate_blocks(n):
+    blocks = 0
+    while n > 0:
+        blocks += n
+        n -= 1
+    print(blocks)
 
-def pyramid_blocks_rec(rows, total = 0):
-    if rows == 0:
-        print(total)
-        return
-    total += rows
-    pyramid_blocks_rec(rows - 1, total)
 
-pyramid_blocks_rec(rows)
+calculate_blocks(rows)
+
+
+# recursively
+def calc_blocks_recursively(n):
+    if n == 1:
+        return 1
+    return n + calc_blocks_recursively(n - 1)
+
+
+print(calc_blocks_recursively(rows))
